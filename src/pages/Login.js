021a5aaaae79fd4fe32feb53774864f237eccfae
Eddie Woods <ewoods6@students.towson.edu>
2021-03-17 18:45:32 -0400

    Current Progress

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Login() {
   return (
     <div className="login">
-      <h1>Login.js</h1>
+      <h1>Login.js in working</h1>
     </div>
   );
 }
